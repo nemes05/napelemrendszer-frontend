a0088f124cc0597ee0188a74a55b32844a6aa17e
nemes <nemes.barnabas020513@gmail.com>
2023-02-24 10:58:07 +0100

    Complete login script 🚧

--- login.js
+++ login.js
@@ -1,17 +1,15 @@
 var http1 = new XMLHttpRequest();
+
 function login(){
-    var username = Document.getElementById("username").value;
-    var password = Document.getElementById("password").value;
-    /*http1.onreadystatechange() = function(){
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    http1.onreadystatechange() = function(){
         if(this.readyState == 4 && this.status == 400){
             console.log();
         }
-    };*/
-
-    console.log(username);
-    console.log(password);
+    };
     
-    /*http1.open("POST","",true);
+    http1.open("POST","",true);
     http1.setRequestHeader("Content-Type", "application/json");
-    http1.send(JSON.stringify({ "username": username, "password": password}));*/
+    http1.send(JSON.stringify({ "username": username, "password": password}));
 }
