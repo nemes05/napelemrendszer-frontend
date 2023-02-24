1b2479be7982dc3992f423b0963d88a1923bfa40
GerboBalazs <nemadommegt@titok.com>
2023-02-24 11:17:07 +0100

    iframek beágyazása scriptbe⚡️

---
+++ functions.js
@@ -0,0 +1,19 @@
+'use strict'
+function addNewPart(){
+    document.getElementById("form_for_data").innerHTML="<iframe src='newPart.html' title='Új alkatrész felvitele' width='100%' height='200px''></iframe>"
+}
+function setPrice(){
+    document.getElementById("form_for_data").innerHTML="<iframe src='setPrice.html' title='Ár módosítás' width='100%' height='200px''></iframe>"
+}
+function listMissingParts(){
+    alert("Jelenleg ez a funkció még nem elérhető.")
+}
+function listReservedParts(){
+    alert("Jelenleg ez a funkció még nem elérhető.")
+}
+function addShippedPart(){
+    alert("Jelenleg ez a funkció még nem elérhető.")
+}
+function boxManagement(){
+    alert("Jelenleg ez a funkció még nem elérhető.")
+}
