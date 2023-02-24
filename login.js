89308d380570d18393a353e28fbc4b6c42249575
nemes <nemes.barnabas020513@gmail.com>
2023-02-24 19:07:15 +0100

    Improved login script :zap:

--- login.js
+++ login.js
@@ -1,15 +1,22 @@
 var http1 = new XMLHttpRequest();
 
 function login(){
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+    //Kiszedi az adatokat a Login formból és JSON-be rendezi
+    var formData = $('#loginForm').serializeArray();
+    var json = {};
+    $.each(formData, function () {
+        json[this.name] = this.value;
+    });
+
+    //Szerver válaszát kezeli
     http1.onreadystatechange() = function(){
         if(this.readyState == 4 && this.status == 400){
             console.log();
         }
     };
     
-    http1.open("POST","",true);
+    //Megnyitja a kommunikációt a szerverrel
+    http1.open("POST","");
     http1.setRequestHeader("Content-Type", "application/json");
-    http1.send(JSON.stringify({ "username": username, "password": password}));
+    http1.send(json);
 }
