d19b9152036190ddf275b5c4ebfb87a6897855f1
nagykrisztian <krisznagy2001@gmail.com>
2023-02-27 23:30:02 +0100

    🚑️Fixes connection error to backend

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 var http1 = new XMLHttpRequest();
 
-function login(){
+function login() {
     //Kiszedi az adatokat a Login formból és JSON-be rendezi
     var formData = $('#loginForm').serializeArray();
     var json = {};
@@ -9,14 +9,16 @@ function login(){
     });
 
     //Szerver válaszát kezeli
-    http1.onreadystatechange() = function(){
-        if(this.readyState == 4 && this.status == 400){
-            console.log();
+    http1.onreadystatechange = function () {
+        if (this.readyState == 4 &&this.status==200) {
+            let response=JSON.parse(this.response);
+            console.log(response);
         }
     };
-    
+
+    console.log(JSON.stringify(json));
     //Megnyitja a kommunikációt a szerverrel
-    http1.open("POST","");
+    http1.open("POST", "http://localhost:3000/login");
     http1.setRequestHeader("Content-Type", "application/json");
-    http1.send(json);
+    http1.send(JSON.stringify(json));
 }
