d07d933db368ccfc3311ab243d2e54b371401d1d
nemes <nemes.barnabas020513@gmail.com>
2023-03-01 17:47:21 +0100

    :sparkles: Implements login script

--- login.js
+++ login.js
@@ -10,13 +10,25 @@ function login() {
 
     //Szerver válaszát kezeli
     http1.onreadystatechange = function () {
-        if (this.readyState == 4 &&this.status==200) {
+        if (this.readyState == 4 && this.status == 200) {
             let response=JSON.parse(this.response);
-            console.log(response);
+            
+            if(response.permission == "Raktarvezeto"){
+                location.replace('storage.html');
+            }
+            if(response.permission == "Szakember"){
+                location.replace('constructor.html');
+            }
+            if(response.permission == "Raktaros"){
+                alert('This user is not set yet!')
+            }
+        }
+        else if(this.readyState == 4 && this.status == 401){
+            console.log("a");
+            alert('Hibás felhasználónév vagy jelszó');
         }
     };
 
-    console.log(JSON.stringify(json));
     //Megnyitja a kommunikációt a szerverrel
     http1.open("POST", "http://localhost:3000/login");
     http1.setRequestHeader("Content-Type", "application/json");
